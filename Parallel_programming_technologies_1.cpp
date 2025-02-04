a831d7c4c1c70ba8f12fef7e638ea026fb56f115
Max <162749285+Cyber-Limon@users.noreply.github.com>
2025-02-04 22:00:14 +0500

    Add files via upload

---
+++ Parallel_programming_technologies_1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cmath>
+
+
+
+using namespace std;
+
+int main() {
+
+	int N = 900000000;
+	double sum = 0;
+	double start_time = clock();
+
+	for (int n = 1; n <= N; n++)
+		sum += pow(-1, n) / (n - log(n));
+
+	double end_time = clock();
+
+	cout << "SUM  = " << sum << "\n";
+	cout << "Time = " << (end_time - start_time) / CLK_TCK << "\n\n\n";
+
+
+
+	return 0;
+}	
